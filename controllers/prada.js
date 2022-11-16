5f342f563e9d1bb1afea3ce67dd2750c631815c3
aishwaryasolleti28 <111617006+aishwaryasolleti28@users.noreply.github.com>
2022-11-16 17:49:19 -0600

    add

--- controllers/prada.js
+++ controllers/prada.js
@@ -5,9 +5,16 @@ exports.prada_list = function(req, res) {
     res.send('NOT IMPLEMENTED: Prada list'); 
 }; 
  
-// for a specific Prada. 
-exports.prada_detail = function(req, res) { 
-    res.send('NOT IMPLEMENTED: Prada detail: ' + req.params.id); 
+// for a specific Prada 
+exports.prada_detail = async function(req, res) { 
+    console.log("detail"  + req.params.id) 
+    try { 
+        result = await Prada.findById( req.params.id) 
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    } 
 }; 
  
 // Handle Prada create on POST. 
@@ -50,7 +57,7 @@ exports.prada_view_all_Page = async function(req, res) {
     }   
 }; 
 
-// Handle Costume create on POST. 
+// Handle Prada create on POST. 
 exports.prada_create_post = async function(req, res) { 
     console.log(req.body) 
     let document = new Prada(); 
@@ -69,4 +76,94 @@ exports.prada_create_post = async function(req, res) {
         res.status(500); 
         res.send(`{"error": ${err}}`); 
     }   
+}; 
+
+// Handle Prada update form on PUT. 
+exports.prada_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body 
+${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await Prada.findById( req.params.id) 
+        // Do updates of properties 
+        if(req.body.color)  
+               toUpdate.color = req.body.color; 
+        if(req.body.cost) toUpdate.cost = req.body.cost; 
+        if(req.body.brand) toUpdate.brand = req.body.brand; 
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} 
+failed`); 
+    } 
+}; 
+
+// Handle Prada delete on DELETE. 
+exports.prada_delete = async function(req, res) { 
+    console.log("delete "  + req.params.id) 
+    try { 
+        result = await Prada.findByIdAndDelete( req.params.id) 
+        console.log("Removed " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": Error deleting ${err}}`); 
+    } 
+}; 
+
+// Handle a show one view with id specified by query 
+exports.prada_view_one_Page = async function(req, res) { 
+    console.log("single view for id "  + req.query.id) 
+    try{ 
+        result = await Prada.findById( req.query.id) 
+        res.render('pradadetail',  
+{ title: 'Prada Detail', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+
+// Handle building the view for creating a prada. 
+// No body, no in path parameter, no query. 
+// Does not need to be async 
+exports.prada_create_Page =  function(req, res) { 
+    console.log("create view") 
+    try{ 
+        res.render('pradacreate', { title: 'Prada Create'}); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+
+// Handle building the view for updating a Prada. 
+// query provides the id 
+exports.prada_update_Page =  async function(req, res) { 
+    console.log("update view for item "+req.query.id) 
+    try{ 
+        let result = await Prada.findById(req.query.id) 
+        res.render('pradaupdate', { title: 'Prada Update', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+
+// Handle a delete one view with id from query 
+exports.prada_delete_Page = async function(req, res) { 
+    console.log("Delete view for id "  + req.query.id) 
+    try{ 
+        result = await Prada.findById(req.query.id) 
+        res.render('pradadelete', { title: 'Prada Delete', toShow: 
+result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
 }; 
--- routes/prada.js
+++ routes/prada.js
@@ -13,3 +13,15 @@ var router = express.Router();
 /* GET pradas */ 
 router.get('/', prada_controlers.prada_view_all_Page ); 
 module.exports = router; 
+
+/* GET detail prada page */ 
+router.get('/detail', prada_controlers.prada_view_one_Page);
+
+ /* GET create prada page */ 
+router.get('/create', prada_controlers.prada_create_Page); 
+ 
+/* GET create update page */ 
+router.get('/update', prada_controlers.prada_update_Page); 
+
+/* GET delete prada page */ 
+router.get('/delete', prada_controlers.prada_delete_Page);
